3462d795b610ffc38dbc972050e9f73c64ed0832
Gabriel Samarane <samarane.gabriel@gmail.com>
2022-02-21 16:22:06 -0300

    Ola

--- src/application/Programming.java
+++ src/application/Programming.java
@@ -3,8 +3,8 @@ package application;
 public class Programming {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Bom dia");
+		System.out.println("Boa Tarde");
 	}
 
 }
